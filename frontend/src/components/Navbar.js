29e84e6541b4c2d801055053747b4b41bc954dba
unknown <HP@Innovation-Sys10.SCAS.COM>
2023-01-13 11:06:32 +0530

    Login test pending

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
+import { logout } from '../actions/auth';
+import { connect } from 'react-redux'
 
-const Navbar = () => {
+const Navbar = ({logout, isAuthenticated}, ) => {
+
+  const guestLinks = () => (
+    <Fragment>
+      <li className="nav-item">
+        <Link className="nav-link" to="/login">
+          Login
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" to="/signup">
+          Sign Up
+        </Link>
+      </li>
+    </Fragment>
+  )
+  const authLinks = () => (
+    <li className="nav-item">
+      <a className="nav-link" href="#!" onClick={logout} >
+        Logout
+      </a>
+    </li>
+  )
+
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
@@ -26,16 +51,8 @@ const Navbar = () => {
                 Home
               </Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/login">
-                Login
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/signup">
-                Sign Up
-              </Link>
-            </li>
+            {isAuthenticated ? authLinks() : guestLinks()}
+            
           </ul>
         </div>
       </div>
@@ -43,4 +60,8 @@ const Navbar = () => {
   )
 }
 
-export default Navbar;
+const mapStateToProps = state =>({
+  isAuthenticated: state.auth.isAuthenticated
+})
+
+export default connect(mapStateToProps, logout) (Navbar);
